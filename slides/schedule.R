b8fe73150b6fa8aaab295e056d5b0246737a2a75
Christoph Adrian <christoph.adrian@fau.de>
2025-10-23 23:41:48 +0200

    Add slides session 01 including metadata for upcoming slides (background images, etc.)

---
+++ slides/schedule.R
@@ -0,0 +1,49 @@
+# DBD Schedule
+## Load packages
+library(jsonlite)
+library(tidyverse)
+
+# Schedule A
+## Create schedule
+schedule_json <- '[
+    {
+        "Session": "1",
+        "Datum": "24.10.2024",
+        "Time": "09:45 - 11:15",
+        "Topic": "🚀 Kick-Off"
+    },
+    {
+        "Session": "",
+        "Datum": "",
+        "Time": "",
+        "Topic": "✏️ Independent study and assignments"
+    },
+    {
+        "Session": "2",
+        "Datum": "09.01.2026",
+        "Time": "09:00 - 16:00",
+        "Topic": "📚 From Theory to Questionaire"
+    },
+    {
+        "Session": "3",
+        "Datum": "23.01.2026",
+        "Time": "09:00 - 16:00",
+        "Topic": "📚 From Questionaire to Pretest"
+    },
+    {
+        "Session": "4",
+        "Datum": "23.01.2026",
+        "Time": "09:00 - 16:00",
+        "Topic": "📚 From Pretest to Analysis"
+    },
+    {
+        "Session": "5",
+        "Datum": "30.01.2026",
+        "Time": "09:00 - 16:00",
+        "Topic": "📚 Analysis & Evaluation"
+
+    }
+]'
+
+## Load schedule to environment
+schedule <- fromJSON(schedule_json) %>% tibble()
